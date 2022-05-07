ea97fc4d3a7348e2067bba0d756ba2a5782363e2
Slimage <105026572+Slimage@users.noreply.github.com>
2022-05-06 23:47:27 -0400

    Update Final Project Dashboard.py

--- Final Project Dashboard.py
+++ Final Project Dashboard.py
@@ -33,28 +33,32 @@ server = app.server
 
 fig = px.bar(df, x="Passport", y= "Mobility Score", color="Passport")
 
-app.layout = html.Div([
-    html.H1('Passport Index',
+app.layout = html.Div(children=[
+    html.H1(children='Passport Index Dashboard',
         style={'textAlign' : 'center'}),
-    html.H2('by Sabrina Limage',
+    html.H2(children='by Sabrina Limage',
         style={'textAlign' : 'center'}),
-    html.H4('What is the Passport Index?',
+    html.H4(children='What is the Passport Index?',
         style={'textAlign' : 'left'}),
-    html.H6('The Passport Index is a passport ranking tool used by visitors to explore different passports around the world. Select a country name to get the mobility score.',
+    html.H6(children='The Passport Index is a passport ranking tool used by global travelers to explore different passports around the world. \
+            It is the only real-time global ranking of the world’s passports, updated as frequently as new visa waivers and changes are implemented.\
+            This dashboard was inspired by the original Passport Index created by Arton Capital.\
+            Select a country name to get the mobility score.',
         style={'textAlign' : 'left'}),
-    dcc.Dropdown(df.Passport, id='pandas-dropdown-1'),
-    html.Div(id='pandas-output-container-1'),
-    dcc.Dropdown(df.VisaFree, id='pandas-dropdown-1'),
-    html.Div(id='pandas-output-container-1'),
-    dcc.Graph(figure= fig)
-    ])
-
-#app.layout = dash_table.DataTable(df.to_dict('records'), [{"Mobility Score": i, "Visa Required": i} for i in df.columns])
+   dcc.Dropdown(df.Passport, id='pandas-dropdown-1'),
+   html.Div(id='pandas-output-container-1'),
+   #dcc.Dropdown(df.VisaFree, id='pandas-dropdown-1'),
+   #html.Div(id='pandas-output-container-1'),
+   dcc.Graph(figure= fig)
+   ])
 
 @app.callback(
     Output('pandas-output-container-1', 'Passport'),
     Input('pandas-dropdown-1', 'value')
-)
+) 
+
+
+
 def update_output(value):
     return f'You have selected {value}'
 
